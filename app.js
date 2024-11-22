8c1e4d06986ef37ef1b676cc918e23deec7ea644
shimwabobs <shimwabonheur00@gmail.com>
2024-11-22 15:21:06 +0200

    check

--- app.js
+++ app.js
@@ -1,64 +1,19 @@
 import http from "http";
 import express from "express";
-import bodyParser from "body-parser";
 import logger from "morgan";
+import bodyParser from "body-parser";
 
+const port=3000;
 const host="localhost";
-const port=3809;
-const app= express();
-const server=http.createServer(app);
+const app=express(http);
 
 app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
-app.post("/add",(req,res)=>{
-    const num1=req.body.num1;
-    const num2=req.body.num2;
-    const sum=num1+num2;
-    res.status(200).json({
-        message:"It works well",
-        result:sum,
-    });
-    
-});
 
-app.post("/sub",(req,res)=>{
-    const num1=req.body.num1;
-    const num2=req.body.num2;
-    const sub=num1-num2;
+app.get("/",(req,res)=>{
     res.status(200).json({
-        result:sub,
-    });
-});
-
-app.post("/product",(req,res)=>{
-    const num1=req.body.num1;
-    const num2=req.body.num2;
-    const prod=num1*num2;
-
-    res.status(200).json({
-        result:prod,
-    });
-});
-
-app.post("/quo",(req,res)=>{
-    const num1=req.body.num1;
-    const num2=req.body.num2;
-    const quo=num1/num2;
-    res.status(200).json({
-        result:quo,
-    });
-})
-
-app.post("/power",(req,res)=>{
-    const num1=req.body.num1;
-    const num2=req.body.num2;
-    const pwer=num1**num2;
-    res.status(200).json({
-        result:pwer,
+        message:"It worke well"
     })
-})
-
-
-server.listen(port,host,()=>{console.log(`Works on ${port} on ${host}`)});
+}).listen(port,host,()=>{console.log("It works")});
